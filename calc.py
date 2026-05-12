657342f9fbb7ed6ebd69b7e15cbd7cd706ba0570
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2026-05-12 15:53:23 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,3 +2,8 @@ def cubic_eq(a, b, c, d):
   b = (-1 * b) / (3 * a)
   c = c / a
   d = b ** 3 - (b * c) / 2 - d / (2 * a)
+  y = b ** 2 - c / 3
+  x = d ** 2 - y ** 3
+  if x <= 0:
+    lbl_1()
+  ans = d + 
