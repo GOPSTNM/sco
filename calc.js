b4d5c793ccadd0d4bf587abf34943e1636fe8bcb
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2026-05-14 15:07:59 +0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -9,17 +9,19 @@ function cubic_eq() {
   let x;
   let y;
   let ans;
-  b = (-1 * b) / (3 * a)
-  c = c / a
-  d = b ** 3 - (b * c) / 2 - d / (2 * a)
-  y = b ** 2 - c / 3
-  global x = d ** 2 - y ** 3
-  if x <= 0:
-    lbl_1()
-  else:
-    global ans = d + x ** (1/2)
-  global ans = ans ** (1/3) - (ans - 2 * d) ** (1/3)
-  lbl_2()
+  b = (-1 * b) / (3 * a);
+  c = c / a;
+  d = b ** 3 - (b * c) / 2 - d / (2 * a);
+  y = b ** 2 - c / 3;
+  x = d ** 2 - y ** 3;
+  if (x <= 0) {
+    lbl_1();
+  } else {
+    ans = d + x ** (1/2);
+  }
+  ans = ans ** (1/3) - (ans - 2 * d) ** (1/3);
+  lbl_2();
+}
 def lbl_1():
   if y:
     global ans = 2 * y ** (1/2) * math.cos((1/3) * math.acos(d / (y ** (3/2))))
