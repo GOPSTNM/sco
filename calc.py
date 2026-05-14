e6dcc967392be2de3f22c9ba8197b8f89867a6bb
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2026-05-14 11:31:19 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,30 +7,30 @@ x = 0
 y = 0
 ans = 0
 def cubic_eq():
-  a = float(input())
-  b = float(input())
-  c = float(input())
-  d = float(input())
-  b = (-1 * b) / (3 * a)
-  c = c / a
-  d = b ** 3 - (b * c) / 2 - d / (2 * a)
-  y = b ** 2 - c / 3
-  x = d ** 2 - y ** 3
+  global a = float(input())
+  global b = float(input())
+  global c = float(input())
+  global d = float(input())
+  global b = (-1 * b) / (3 * a)
+  global c = c / a
+  global d = b ** 3 - (b * c) / 2 - d / (2 * a)
+  global y = b ** 2 - c / 3
+  global x = d ** 2 - y ** 3
   if x <= 0:
     lbl_1()
   else:
-    ans = d + x ** (1/2)
-  ans = ans ** (1/3) - (ans - 2 * d) ** (1/3)
+    global ans = d + x ** (1/2)
+  global ans = ans ** (1/3) - (ans - 2 * d) ** (1/3)
   lbl_2()
 def lbl_1():
   if y:
-    ans = 2 * y ** (1/2) * math.cos((1/3) * math.acos(d / (y ** (3/2))))
+    global ans = 2 * y ** (1/2) * math.cos((1/3) * math.acos(d / (y ** (3/2))))
 def lbl_2():
-  a = b + ans
+  global a = b + ans
   print(a)
-  d = 3 * b - a
-  b = d / 2 + (d ** 2 / 4 - c + a * d) ** 1/2
+  global d = 3 * b - a
+  global b = d / 2 + (d ** 2 / 4 - c + a * d) ** 1/2
   print(b)
-  c = d - b
+  global c = d - b
   print(c)
 cubic_eq()
