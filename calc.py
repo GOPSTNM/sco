11f6e64ad1a2b19d5c6880b1733da599365e808f
GOPSTNM <166233545+GOPSTNM@users.noreply.github.com>
2026-05-14 09:51:27 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -17,11 +17,11 @@ def cubic_eq():
   y = b ** 2 - c / 3
   x = d ** 2 - y ** 3
   if x <= 0:
-    lbl_1()
+    lbl_1(a, b, c, d, x, y)
   else:
     ans = d + x ** (1/2)
   ans = ans ** (1/3) - (ans - 2 * d) ** (1/3)
-  lbl_2()
+  lbl_2(a, b, c, d, x, y)
 def lbl_1():
   if y:
     ans = 2 * y ** (1/2) * math.cos((1/3) * math.acos(d / (y ** (3/2))))
